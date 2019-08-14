1d21a697f619262f5fe84a47e397fb84757105f6
PaVeLoR <@DNAPC.sense.kharkov.ua>
2019-08-14 21:00:30 +0300

    ua iban

--- src/IBAN_Utils.pas
+++ src/IBAN_Utils.pas
@@ -11,6 +11,8 @@ type
     // Genera un IBAN para una CCC. EJ: "ES17"
     class function GetIBAN(inSiglaPais, inCCC: string): string;
 
+    class function GetUAIBan(Mfo: string; Acc: string): string;
+
     // Valida un IBAN (Generico todos los paises UE)
     class function IsValidIBAN(inFull: String; Errores: TStringList=nil): Boolean;
   end;
@@ -31,6 +33,19 @@ begin
   IBAN.DC := TrBancoIBANInfo_GetDigitoControl(IBan, inCCC);
 
   Result := TrBancoIBANInfo_ToIBAN(IBan);
+end;
+
+class function TIBANUtils.GetUAIBan(Mfo, Acc: string): string;
+var
+  S: string;
+begin
+  SetLength(S, 25);
+  FillChar(S[1], 25, '0');
+  if Length(Mfo) > 0 then
+    Move(Mfo[1], S[1], Length(Mfo));
+  if Length(Acc) > 0 then
+    Move(Acc[1], S[Length(S) - Length(Acc) + 1], Length(Acc));
+  Result := TIBANUtils.GetIBAN('UA', S) + S;
 end;
 
 class function TIBANUtils.IsValidIBAN(inFull: String; Errores: TStringList=nil): Boolean;
